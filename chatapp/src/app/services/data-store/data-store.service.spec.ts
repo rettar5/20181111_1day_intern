d7906d8528c5cf2a191227ccf170008231cf4ff8
sichinose <s_ichinose@dreamarts.co.jp>
2018-10-30 14:09:38 +0900

    Added unit tests at DataStoreService.

--- chatapp/src/app/services/data-store/data-store.service.spec.ts
+++ chatapp/src/app/services/data-store/data-store.service.spec.ts
@@ -1,7 +1,86 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DataStoreService } from './data-store.service';
+import { DataStoreService, LocalStorageKey } from './data-store.service';
 
 describe('DataStoreService', () => {
+  const str = Math.floor(Math.random() * 10000).toString();
+  const obj = { str: str, ary: [ str ] };
+  const lang = 'en';
+  const strKey: LocalStorageKey = (LocalStorageKey.prefix + '.test.str') as LocalStorageKey;
+  const objKey: LocalStorageKey = (LocalStorageKey.prefix + '.test.obj') as LocalStorageKey;
+
   beforeEach(() => TestBed.configureTestingModule({}));
+
+  describe('setItem()', () => {
+    it('文字列がローカルストレージに保存できること', () => {
+      DataStoreService.setItem(strKey, str);
+      expect(localStorage.getItem(strKey)).toBe(str);
+    });
+
+    it('オブジェクトがローカルストレージに保存できること', () => {
+      DataStoreService.setItem(objKey, JSON.stringify(obj));
+      expect(localStorage.getItem(objKey)).toBe(JSON.stringify(obj));
+    });
+  });
+
+  describe('getItem()', () => {
+    it('ローカルストレージに保存された文字列が取得できること', () => {
+      const item = DataStoreService.getItem(strKey);
+      expect(item).toBe(str);
+    });
+
+    it('ローカルストレージに保存されたオブジェクトが取得できること', () => {
+      const item = DataStoreService.getItem(objKey);
+      expect(item).toBe(JSON.stringify(obj));
+    });
+  });
+
+  describe('removeItem()', () => {
+    it('ローカルストレージに保存された文字列が削除できること', () => {
+      const step1st = DataStoreService.getItem(strKey);
+      expect(step1st).toBe(str);
+      DataStoreService.removeItem(strKey);
+      const step2nd = DataStoreService.getItem(strKey);
+      expect(step2nd).toBeNull();
+    });
+
+    it('ローカルストレージに保存されたオブジェクトが削除できること', () => {
+      const step1st = DataStoreService.getItem(objKey);
+      expect(step1st).toBe(JSON.stringify(obj));
+      DataStoreService.removeItem(objKey);
+      const step2nd = DataStoreService.getItem(objKey);
+      expect(step2nd).toBeNull();
+    });
+  });
+
+  describe('clear()', () => {
+    it('ローカルストレージに保存された文字列が削除できること', () => {
+      DataStoreService.setItem(strKey, str);
+      const step1st = DataStoreService.getItem(strKey);
+      expect(step1st).toBe(str);
+      DataStoreService.clear();
+      const step2nd = DataStoreService.getItem(strKey);
+      expect(step2nd).toBeNull();
+    });
+
+    it('ローカルストレージに保存されたオブジェクトが削除できること', () => {
+      DataStoreService.setItem(objKey, JSON.stringify(obj));
+      const step1st = DataStoreService.getItem(objKey);
+      expect(step1st).toBe(JSON.stringify(obj));
+      DataStoreService.clear();
+      const step2nd = DataStoreService.getItem(objKey);
+      expect(step2nd).toBeNull();
+    });
+
+    it('ローカルストレージのクリア時に、言語設定が削除されないこと', () => {
+      DataStoreService.setItem(strKey, str);
+      DataStoreService.setItem(LocalStorageKey.lang, lang);
+      expect(DataStoreService.getItem(strKey)).toBe(str);
+      expect(DataStoreService.getItem(LocalStorageKey.lang)).toBe(lang);
+
+      DataStoreService.clear();
+      expect(DataStoreService.getItem(strKey)).toBeNull();
+      expect(DataStoreService.getItem(LocalStorageKey.lang)).toBe(lang);
+    });
+  });
 });
