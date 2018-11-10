92ffb5ef5c1b9a27fbaa0d444e459bbefe733a42
sichinose <s_ichinose@dreamarts.co.jp>
2018-11-10 22:34:42 +0900

    Hidden register buttons and form control.

--- chatapp/src/app/components/groups/groups.component.ts
+++ chatapp/src/app/components/groups/groups.component.ts
@@ -148,7 +148,7 @@ export class GroupsComponent extends BaseComponent implements OnInit {
   styleUrls: [ './groups.component.scss' ]
 })
 export class GroupRegisterDialogComponent {
-  control: FormControl = new FormControl(this.data.name, [Validators.required, Validators.maxLength(12)]);
+  // TODO: 入力必須・文字数制限の実装
 
   constructor(private dialogRef: MatDialogRef<GroupRegisterDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
